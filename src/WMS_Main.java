5e6f9cad709d0b03503c8f10e4fa107c614a1171
N4melessOne <gamerlacikaaa@gmail.com>
2022-04-20 22:13:52 +0200

    Main window name has been changed.

--- src/WMS_Main.java
+++ src/WMS_Main.java
@@ -3,12 +3,14 @@ import javax.swing.*;
 public class WMS_Main extends JFrame{
     private JPanel WMS_MainWindow;
     private JLabel PlantsLbl;
-    private JList list1;
+    private JList PlantsList;
+    private JLabel EnterpriseNameLbl;
 
     public static void main(String[] args) {
         JFrame WMSMainWindow = new JFrame("WMS_Main");
-        WMSMainWindow.setVisible(true);
-        WMSMainWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        WMSMainWindow.setContentPane(new WMS_Main().WMS_MainWindow);
         WMSMainWindow.pack();
+        WMSMainWindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        WMSMainWindow.setVisible(true);
     }
 }
