32310d51f4b240d76ae26f430cf63d05904e85bb
N4melessOne <gamerlacikaaa@gmail.com>
2022-04-20 23:54:10 +0200

    Main window name has been changed.

--- src/WMS_Main.java
+++ src/WMS_Main.java
@@ -1,21 +1,62 @@
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.*;
 
 public class WMS_Main extends JFrame{
     private JPanel WMS_MainWindow;
     private JTree plantsTree;
-    private JLabel entrprNameLbl;
+    private JLabel enterpriseNameLbl;
     private JLabel plantsLbl;
+
 
     public WMS_Main(){
         setContentPane(WMS_MainWindow);
         setTitle("WMS Main Window");
         setSize(1920, 1080);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        createUIComponents();
         setVisible(true);
+    }
+
+    public static DefaultMutableTreeNode getPlantsTree(){
+        DefaultMutableTreeNode plant0 = new DefaultMutableTreeNode("Plant0");
+        DefaultMutableTreeNode div0 = new DefaultMutableTreeNode("division0");
+        DefaultMutableTreeNode div1 = new DefaultMutableTreeNode("division1");
+        DefaultMutableTreeNode div2 = new DefaultMutableTreeNode("division2");
+        plant0.add(div0);
+        plant0.add(div1);
+        plant0.add(div2);
+
+        DefaultMutableTreeNode plant1= new DefaultMutableTreeNode("Plant1");
+        DefaultMutableTreeNode div3 = new DefaultMutableTreeNode("division3");
+        DefaultMutableTreeNode div4 = new DefaultMutableTreeNode("division4");
+        DefaultMutableTreeNode div5 = new DefaultMutableTreeNode("division5");
+        plant1.add(div3);
+        plant1.add(div4);
+        plant1.add(div5);
+
+        DefaultMutableTreeNode plant2 = new DefaultMutableTreeNode("Plant2");
+        DefaultMutableTreeNode div6 = new DefaultMutableTreeNode("division6");
+        DefaultMutableTreeNode div7 = new DefaultMutableTreeNode("division7");
+        DefaultMutableTreeNode div8 = new DefaultMutableTreeNode("division8");
+        plant2.add(div6);
+        plant2.add(div7);
+        plant2.add(div8);
+
+        DefaultMutableTreeNode plants = new DefaultMutableTreeNode("Plants");
+        plants.add(plant0);
+        plants.add(plant1);
+        plants.add(plant2);
+
+        return plants;
     }
 
     public static void main(String[] args) {
         JFrame WMSMainWindow = new WMS_Main();
         WMSMainWindow.pack();
+    }
+
+    private void createUIComponents() {
+        plantsTree = new JTree(WMS_Main.getPlantsTree());
     }
 }
