a726d62ff63d338456a3b8ca02611b15d808d224
d4rkside111 <sidlovszki.dominik@gmail.com>
2022-05-17 21:45:05 +0200

    test

--- src/WMS_LoginForm.java
+++ src/WMS_LoginForm.java
@@ -1,15 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class WMS_LoginForm extends JFrame{
     private JPanel WMS_LoginForm;
-    private JLabel testLbl;
+    private JTextField textField1;
+    private JLabel idLbl;
+    private JButton button1;
 
     public WMS_LoginForm(){
         setContentPane(WMS_LoginForm);
         setTitle("WMS Login As Leader");
-        setSize(640, 480);
+        //Dimension dimension = new Dimension(320,240);
+        //setMaximumSize(dimension);
+        setLocationRelativeTo(null);
+        setSize(320,200);
+        setResizable(false);
+
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
+
     }
 
     public static void main(String[] args) {
