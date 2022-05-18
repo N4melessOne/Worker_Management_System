93fd15e2152f9802d517f658ac77dbb070717581
d4rkside111 <sidlovszki.dominik@gmail.com>
2022-05-18 22:08:51 +0200

    test

--- src/WMS_AddWorker.java
+++ src/WMS_AddWorker.java
@@ -4,15 +4,31 @@ public class WMS_AddWorker extends JFrame{
     private JPanel WMS_AddWorker;
     private JTextField workerNameTb;
     private JLabel workerNameLbl;
+    private JTextField addressTb;
+    private JTextField departmentIdTb;
+    private JTextField positionTb;
+    private JTextField mobileTb;
+    private JTextField emailTb;
+    private JTextField salaryTb;
+    private JTextField textField8;
+    private JButton addWorkerBtn;
+    private JLabel workerAddressLbl;
+    private JLabel birthDateLbl;
+    private JLabel departmentIdLbl;
+    private JLabel isLeaderLbl;
+    private JLabel mobileLbl;
+    private JLabel emailLbl;
+    private JLabel salaryLbl;
+    private JTextField birthDateTb;
 
     public WMS_AddWorker(){
         setContentPane(WMS_AddWorker);
         setTitle("WMS Add Worker");
-        setLocationRelativeTo(null);
         setSize(400,600);
         setResizable(false);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
+        setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
